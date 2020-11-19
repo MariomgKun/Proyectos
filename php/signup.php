0a306b4615d5bf6f63b7e37a6fdc6fb2e9d6d7f5
NoelAndrew <andrew98noel@gmail.com>
2020-11-19 10:04:32 -0600

    hotfix/ sing up

--- php/signup.php
+++ php/signup.php
@@ -32,7 +32,7 @@
   if (mysqli_query($conexion, $query)) {
     session_start();
     $_SESSION["usuario"] =$gamertag ;
-    header("location: ../principal.html");
+    header("location: principal.php");
   } else {
     echo "Error: " . $query . "<br>" . mysqli_error($conexion);
   }
