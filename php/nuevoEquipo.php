e89ae0a98b98f590917e06d96b0dc176665f6a71
NoelAndrew <andrew98noel@gmail.com>
2020-11-19 16:46:45 -0600

    bugfix/ database 2

---
+++ php/nuevoEquipo.php
@@ -0,0 +1,30 @@
+<?php
+  include("Conexion.php");
+
+    $TeamName = $_POST['TeamName'];
+    $j1 = $_POST['jugador1'];
+    $j2 = $_POST['jugador2'];
+    //$j3 = $_POST['jugador3'];
+    //$j4 = $_POST['jugador4'];
+
+  $query = "INSERT INTO equipo(NombreEquip,JugadorUno,JugadorDos) VALUES('$TeamName','$j1','$j2')";
+  //WHERE Gamertag = '$j1'";
+  
+  $resultado= $conexion->query($query);
+  if (mysqli_query($conexion, $query)) {
+    session_start();
+    $_SESSION["usuario"] =$gamertag ;
+    header("location: principal.php");
+  } else {
+    echo "Error: " . $query . "<br>" . mysqli_error($conexion);
+  }
+  
+  $resultado= $conexion->query2($query2);
+  if (mysqli_query($conexion, $query2)) {
+    session_start();
+    $_SESSION["usuario"] =$gamertag ;
+    header("location: principal.php");
+  } else {
+    echo "Error: " . $query2 . "<br>" . mysqli_error($conexion);
+  }
+  ?>
