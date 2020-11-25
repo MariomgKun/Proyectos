298bccf264cf487223b08c61ab64dec6aa368af8
NoelAndrew <andrew98noel@gmail.com>
2020-11-25 12:58:47 -0600

    feature/ mostrar perfil

--- php/perfil.php
+++ php/perfil.php
@@ -7,73 +7,63 @@
 	<link rel="stylesheet" href="../css/perfil.css">
 	<link rel="stylesheet" href="../js/jquery-3.5.1.min.js">
 	<link rel="stylesheet" href="../js/semantic.js">
-    <link rel="stylesheet" href="../js/semantic.min.js">
-    <script src="https://code.jquery.com/jquery-3.1.1.min.js"
-		integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
-		crossorigin="anonymous"></script>
-	<script src="js/semantic.min.js"></script>
+	<link rel="stylesheet" href="../js/semantic.min.js">
 </head>
 <body>
 	<!--Menu superior-->
 	<div class="ui menu">
-		<a class="item" href="principal.php">WolfBattles</a>
-	</div>
+		<a class="item" href="index.html">WolfBattles</a>
+    </div>
+    <?php
+      include("Conexion.php");
+        session_start();
+        $aux= $_SESSION['usuario'];
+        echo "<a class='item' > $aux </a>"; 
+
+        ?>
 
 	<div class="contForm">
         <div class="contPortada">
             <div class="ui black four item inverted menu">
 				<a class="item">Juegos</a>
-				<a class="item" href="torneos.php">Torneos</a>
-                <a class="item" href="clasificacion.php">Clasificaciones</a>
-				<a class="item" href="perfil.php">Cuenta</a>
+				<a class="item" href="torneos.html">Torneos</a>
+                <a class="item" href="clasificacion.html">Clasificaciones</a>
+                <a class="item" href="perfil.html">Cuenta</a>
 			</div>
 			<div class="contForm">
 				<div class="sigInContainer">
-					
 					<div class="ui top attached tabular menu">
-						<a class="active item" data-tab="first">Perfil</a>
-						<a class="item" data-tab="second">Información</a>
-					</div>
-					<div class="ui bottom attached active tab segment" data-tab="first">
-						<div class="ui raised segments">
-							<div class="ui segment">
-							  <p>Nombre</p>
-							</div>
-							<div class="ui segment">
-							  <p>Id</p>
-							</div>
-							<div class="ui segment">
-							  <p>Gamertag</p>
-							</div>
-						</div>
-					</div>
-					<div class="ui bottom attached tab segment" data-tab="second">
-						<div class="ui raised segments">
-							<div class="ui segment">
-							  <p>Top</p>
-							</div>
-							<div class="ui segment">
-							  <p>Medio</p>
-							</div>
-							<div class="ui segment">
-							  <p>Bottom</p>
-							</div>
-						</div>
-					</div>
+						<a class="item active"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">
+						  Perfil
+                        </font>
+                    </font>   
+                    </a>
+                     </div>
+                          <?php
+                         $query = "SELECT * FROM perfil WHERE Gamertag= '$aux'";
+                         $resultado= $conexion->query($query);
 
-				</div>
-			</div>
-
+                        if ($resultado->num_rows > 0) {
+                            while($row = $resultado->fetch_assoc()) {
+                            echo "id: " . $row["idPerfil"]."</br>Nombre: " . $row["Nombre"]. " " . $row["Apellido"]. "</br>Email: " . $row["Email"]. "</br>Gamertag: " . $row["Gamertag"]. "<br>";
+                            }
+                        } else {
+                            echo "0 results";
+                        }
+                        ?>
+						</div>
+					  </div>
+					  <div class="ui bottom attached segment">
+						<p></p>
+					  </div>
 		</div>
-        
+		</div>
+		</div>
+
     </div>
-	
+
 	<footer id="main-footer">
         <p>WolfBattles&copy; 2020, Derechos reservados</p>
-	</footer>
+    </footer>
 </body>
-<script>
-	$('.menu .item').tab();
-	$('.tabular.menu .item').tab();
-</script>
 </html>
--- php/signup.php
+++ php/signup.php
@@ -1,39 +1,69 @@
-<?php
-  include("Conexion.php");
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Perfil</title>
+	<link rel="stylesheet" href="../css/semantic.min.css">
+	<link rel="stylesheet" href="../css/semantic.css">
+	<link rel="stylesheet" href="../css/perfil.css">
+	<link rel="stylesheet" href="../js/jquery-3.5.1.min.js">
+	<link rel="stylesheet" href="../js/semantic.js">
+	<link rel="stylesheet" href="../js/semantic.min.js">
+</head>
+<body>
+	<!--Menu superior-->
+	<div class="ui menu">
+		<a class="item" href="index.html">WolfBattles</a>
+    </div>
+    <?php
+      include("Conexion.php");
+        session_start();
+        $aux= $_SESSION['usuario'];
+        echo "<a class='item' > $aux </a>"; 
 
+        ?>
 
-    $email = $_POST['Username'];
-    $contraseña = $_POST['Password'];
-    $gamertag = $_POST['Gamertag'];
-    $contrasenia = $_POST['Password'];
-    
-      switch ($_POST["Plataforma"]){
-        case 0:
-           $plataf = "Mobile";
-        break;
-        case 1:
-          $plataf = "PC";
-        break;
-       case 2:
-        $plataf = "Xbox";
-        break;
-        case 2:
-          $plataf = "Play Station";
-          break;
+	<div class="contForm">
+        <div class="contPortada">
+            <div class="ui black four item inverted menu">
+				<a class="item">Juegos</a>
+				<a class="item" href="torneos.html">Torneos</a>
+                <a class="item" href="clasificacion.html">Clasificaciones</a>
+                <a class="item" href="perfil.html">Cuenta</a>
+			</div>
+			<div class="contForm">
+				<div class="sigInContainer">
+					<div class="ui top attached tabular menu">
+						<a class="item active"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">
+						  Perfil
+                        </font>
+                    </font>   
+                    </a>
+                     </div>
+                          <?php
+                         $query = "SELECT * FROM perfil WHERE Gamertag= '$aux'";
+                         $resultado= $conexion->query($query);
 
-      }
+                        if ($resultado->num_rows > 0) {
+                            while($row = $resultado->fetch_assoc()) {
+                            echo "id: " . $row["idPerfil"]."</br>Nombre: " . $row["Nombre"]. " " . $row["Apellido"]. "</br>Email: " . $row["Email"]. "</br>Gamertag: " . $row["Gamertag"]. "<br>";
+                            }
+                        } else {
+                            echo "0 results";
+                        }
+                        ?>
+						</div>
+					  </div>
+					  <div class="ui bottom attached segment">
+						<p></p>
+					  </div>
+		</div>
+		</div>
+		</div>
 
-    $nombre = $_POST['First_Name'];
-    $apellido = $_POST['Segundo_Nombre'];
+    </div>
 
-  $query = "INSERT INTO perfil(Email,Contraseña,Gamertag,Plataforma,Nombre,Apellido) VALUES('$email','$contraseña','$gamertag','$plataf','$nombre','$apellido')";
-
-
-  if (mysqli_query($conexion, $query)) {
-    session_start();
-    $_SESSION["Gamertag"] =$gamertag ;
-    header("location: principal.php");
-  } else {
-    echo "Error: " . $query . "<br>" . mysqli_error($conexion);
-  }
-  ?>
+	<footer id="main-footer">
+        <p>WolfBattles&copy; 2020, Derechos reservados</p>
+    </footer>
+</body>
+</html> 
